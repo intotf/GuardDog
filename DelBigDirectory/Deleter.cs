34b80b7e04669610fdc4350d303aec2bd0eb8fd2
intotf <intotf@163.com>
2017-11-03 22:14:57 +0800

    并行

--- DelBigDirectory/Deleter.cs
+++ DelBigDirectory/Deleter.cs
@@ -14,11 +14,9 @@ namespace DelBigDirectory
     {
         private static DeleteConfig config = DeleteConfig.instance;
         private static Timer timer;
-        private static int delNum = 0;     //删除文件数
+        private static long delNum = 0;     //删除文件数
         private static int ignoreNum = 0;    //忽略文件数
         private static Stopwatch sw = new Stopwatch();  //运行时间
-        //待删除文件
-        private static List<string> delFiles = new List<string>();
 
         /// <summary>
         /// 初始化服务
@@ -54,6 +52,18 @@ namespace DelBigDirectory
             Console.WriteLine("{0} 执行完毕.总共耗时 {1} ms,删除文件{2},跳过{3}.", DateTime.Now, sw.ElapsedMilliseconds, delNum, ignoreNum);
         }
 
+
+        private static void DelFiles(List<string> delFiles)
+        {
+            Parallel.ForEach(delFiles, (f) =>
+            {
+                Interlocked.Add(ref delNum, 1);
+                File.Delete(f);
+                Console.WriteLine("删除 {0}", f);
+            });
+            delFiles.Clear();
+        }
+
         /// <summary>
         /// 删除单个文件
         /// </summary>
@@ -64,27 +74,22 @@ namespace DelBigDirectory
             {
                 if (config.delFalg)
                 {
+                    var delFiles = new List<string>();
                     delFiles.Add(file.FullName);
                     if (delFiles.Count() > 1000)
                     {
                         Console.WriteLine("累积需要删除的文件达到了{0}条了,开始并行删除", delFiles.Count());
-                        Parallel.ForEach(delFiles, (f) =>
-                        {
-                            Interlocked.Add(ref delNum, 1);
-                            File.Delete(f);
-                        });
+                        DelFiles(delFiles);
+                        delFiles.Clear();
                     }
-                    //File.Delete(file.FullName);
-                    delFiles.Clear();
                 }
-                Console.WriteLine("删除 {0} {1}", file.CreationTime, file.Name);
             }
             else
             {
                 Console.WriteLine("跳过 {0} {1} ", file.CreationTime, file.Name);
                 ignoreNum++;
             }
-            Console.Title = string.Format("删除{0} 跳过{1},运行时长 {2} ms.", delNum, ignoreNum, sw.ElapsedMilliseconds);
+            Console.Title = string.Format("删除{0} 跳过{1},运行时长 {2} ms.", Interlocked.Read(ref delNum), ignoreNum, sw.ElapsedMilliseconds);
         }
 
 
@@ -95,9 +100,10 @@ namespace DelBigDirectory
         /// <returns></returns>
         private static bool Filefilter(FileInfo file)
         {
-            return file.FullName.Substring(file.FullName.Length - 2, 2) != ".." &&
-                           config.filter.Contains(file.Extension) &&
-                           file.CreationTime.AddDays(config.days) < config.now;
+            return true;
+            //file.FullName.Substring(file.FullName.Length - 2, 2) != ".." &&
+            //               config.filter.Contains(file.Extension) &&
+            //               file.CreationTime.AddDays(config.days) < config.now;
         }
     }
 }

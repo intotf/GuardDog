7852073da36c6ee7d7b235b05f55ca10ef9d9388
intotf <intotf@163.com>
2017-11-05 00:20:04 +0800

    修改bug

--- DelBigDirectory/Deleter.cs
+++ DelBigDirectory/Deleter.cs
@@ -33,19 +33,24 @@ namespace DelBigDirectory
                     Deleter.DelAllFile();
                 }, null, dueTime, period);
             }
+        }
 
-
+        /// <summary>
+        /// 间隔1秒输出执行结果
+        /// </summary>
+        private static void ConsoleWrite()
+        {
             ////// 第一个参数是：回调方法，表示要定时执行的方法，
             ////// 第二个参数是：回调方法要使用的信息的对象，或者为空引用，
             ////// 第三个参数是：调用 callback 之前延迟的时间量（以毫秒为单位），指定 Timeout.Infinite 以防止计时器开始计时。指定零 (0) 以立即启动计时器。第四个参数是：定时的时间时隔，以毫秒为单位
-            //if (Deleter.TitleTimer == null)
-            //{
-            //    var dueTilteTime = DateTime.Now.AddHours(config.IntervalTime).Subtract(DateTime.Now);
-            //    Deleter.TitleTimer = new Timer((state) =>
-            //    {
-            //        Console.Title = string.Format("当前耗时 {0} ms,删除{1},跳过{2}.", sw.ElapsedMilliseconds, delNum, ignoreNum);
-            //    }, null, dueTilteTime, TimeSpan.FromSeconds(1));
-            //}
+            if (Deleter.TitleTimer == null)
+            {
+                var dueTilteTime = DateTime.Now.AddSeconds(1).Subtract(DateTime.Now);
+                Deleter.TitleTimer = new Timer((state) =>
+                {
+                    Console.WriteLine("当前耗时 {0} ms,删除{1},跳过{2}.", sw.ElapsedMilliseconds, delNum, ignoreNum);
+                }, null, dueTilteTime, TimeSpan.FromSeconds(1));
+            }
         }
 
         /// <summary>
@@ -53,17 +58,30 @@ namespace DelBigDirectory
         /// </summary>
         public static void DelAllFile()
         {
-            sw.Restart();
+            Dispose();
             var dir = new System.IO.DirectoryInfo(config.dir);
             var files = dir.EnumerateFiles("*.*", SearchOption.AllDirectories);
             foreach (var item in files)
             {
-                Thread.Sleep(1);
                 DelFile(item);
             }
             sw.Stop();
+            Deleter.TitleTimer.Dispose();
+            Deleter.TitleTimer = null;
             Console.WriteLine("{0} 执行完毕,总共耗时 {1}ms,删除 {2},跳过 {3}.", DateTime.Now, sw.ElapsedMilliseconds, delNum, ignoreNum);
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        private static void Dispose()
+        {
+            Console.WriteLine("{0} 开始一轮新的执行", DateTime.Now);
+            delNum = 0;
+            ignoreNum = 0;
+            sw.Restart();
+            ConsoleWrite();
         }
 
         /// <summary>

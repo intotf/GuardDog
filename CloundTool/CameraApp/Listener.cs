e92fd09db0ef635a38e72d5cbb17dcaaa1e88987
intotf <intotf@163.com>
2018-03-31 00:31:56 +0800

    增加Listener

---
+++ CloundTool/CameraApp/Listener.cs
@@ -0,0 +1,20 @@
+﻿using NetworkSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraApp
+{
+    /// <summary>
+    /// 连接类
+    /// </summary>
+    public class Listener
+    {
+        /// <summary>
+        /// WebSocket 连接
+        /// </summary>
+        public static TcpListener WebSocketListener = new TcpListener();
+    }
+}

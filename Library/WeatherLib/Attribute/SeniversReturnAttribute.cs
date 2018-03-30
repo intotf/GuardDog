8b7b603453d18f923317755f241050f8a7f86dc2
intotf@163.com <intotf@163.com>
2018-03-30 13:51:59 +0800

    心知天气包装

---
+++ Library/WeatherLib/Attribute/SeniversReturnAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.Attributes;
+using WebApiClient.Contexts;
+
+namespace WeatherLib
+{
+    /// <summary>
+    /// 查询结果处理
+    /// </summary>
+    public class SeniversReturnAttribute : ApiReturnAttribute
+    {
+        public SeniversReturnAttribute()
+        {
+
+        }
+
+        /// <summary>
+        /// 对接口返回错误码进行转换
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        protected async override Task<object> GetTaskResult(ApiActionContext context)
+        {
+            var txt = await context.ResponseMessage.Content.ReadAsStringAsync();
+            if (txt.Contains("status_code"))
+            {
+                var err = (ResultError)context.HttpApiConfig.JsonFormatter.Deserialize(txt, typeof(ResultError));
+                throw new SeniverserException(err.status_code);
+            }
+            var data = context.HttpApiConfig.JsonFormatter.Deserialize(txt, context.ApiActionDescriptor.Return.DataType);
+            return data;
+        }
+    }
+}

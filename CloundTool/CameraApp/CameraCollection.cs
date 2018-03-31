d712c13150d5463f090287df59a8fb0319ed6be0
intotf <intotf@163.com>
2018-03-31 22:13:35 +0800

    不可用迭代器

--- CloundTool/CameraApp/CameraCollection.cs
+++ CloundTool/CameraApp/CameraCollection.cs
@@ -18,7 +18,8 @@ namespace CameraApp
         /// <summary>
         /// 摄像头集合
         /// </summary>
-        public IEnumerable<Camera> Cameras { get; private set; }
+        public List<Camera> Cameras { get; private set; }
+
 
         /// <summary>
         /// 实例化摄像头
@@ -37,8 +38,9 @@ namespace CameraApp
         /// 获取所有摄像头
         /// </summary>
         /// <returns></returns>
-        private IEnumerable<Camera> GetCameras()
+        private List<Camera> GetCameras()
         {
+            var listCamera = new List<Camera>();
             //获取当前电脑所有摄像头
             var CamerasCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
             for (var i = 0; i < CamerasCollection.Count; i++)
@@ -46,8 +48,9 @@ namespace CameraApp
                 var ca = CamerasCollection[i];
                 var model = new Camera(ca);
                 model.OnRead += CameraApi.CameraReader_OnRead;
-                yield return model;
+                listCamera.Add(model);
             }
+            return listCamera;
         }
     }
 }

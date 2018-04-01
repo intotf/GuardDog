4e32b647a7f27d46c573d528db39cc71ac8abaca
intotf <intotf@163.com>
2018-04-01 22:27:40 +0800

    增加 IndexController

---
+++ CloundTool/CameraApp/IndexController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkSocket.Http;
+
+namespace CameraApp
+{
+    public class IndexController : HttpController
+    {
+        [Route("/")]
+        public ActionResult Index()
+        {
+            Response.Status = 301;
+            Response.Headers.Add("Location ", "/Script/index.html");
+            return new EmptyResult();
+        }
+    }
+}

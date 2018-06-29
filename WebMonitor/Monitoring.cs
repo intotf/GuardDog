ecf19c41e3a2d2d109ead39bc3bd9a8b39367280
皓月青峰 <intotf@163.com>
2018-06-29 11:12:58 +0800

    Update Monitoring.cs
    增加邮件发送间隔

--- WebMonitor/Monitoring.cs
+++ WebMonitor/Monitoring.cs
@@ -1,6 +1,7 @@
-﻿﻿using Infrastructure.Utility;
+﻿using Infrastructure.Utility;
 using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Threading;
@@ -23,6 +24,9 @@ namespace WebMonitor
         {
             this.Name = name;
             this.Url = url;
+            this.Attempts = 0;
+            this.State = true;
+            this.NoticeInterval = 1;
         }
 
 
@@ -39,78 +43,110 @@ namespace WebMonitor
 
             var config = Config.Instance;
 
+            var client = new HttpClient(config.TimeOut * 1000);
+            var ping = new Ping();
+
             //执行监测并自动启动服务
             while (!cancellationToken.IsCancellationRequested)
             {
-                var ping = new Ping();
+                #region 检测当前环境网格
                 try
                 {
                     var pingSatet = await ping.SendPingAsync("www.baidu.com", 1000);
                     if (pingSatet.Status != IPStatus.Success)
                     {
                         this.Attempts = 0;
-                        Debugger.WriteLine("{0} {1}", DateTime.Now, "当前网络连接失败,或网络不稳定.");
+                        Console.WriteLine("{0} {1}", DateTime.Now, "当前网络连接失败,或网络不稳定.");
                         await TaskDelay();
                         continue;
                     }
                 }
                 catch (Exception ex)
                 {
-                    Debugger.WriteLine("{0} {1} {2}", DateTime.Now, "当前网络连接失败", ex.Message);
+                    Console.WriteLine("{0} {1} {2}", DateTime.Now, "当前网络连接失败", ex.Message);
                     Thread.Sleep(5000);
                     continue;
                 }
-                using (var client = new HttpClient(config.TimeOut * 1000))
-                {
-                    var exMsg = "正常";
-                    string result = string.Empty;
-                    var model = new Webs() { Name = this.Name, Url = this.Url, Attempts = 0, State = true };
-                    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();  //运行时间
-                    try
-                    {
-                        sw.Reset();
-                        sw.Start();
-                        result = await client.DownloadStringTaskAsync(this.Url);
-                        sw.Stop();
-                        var oldModel = webList.GetOrAdd(this.Url, model);
-                        if (!oldModel.State && oldModel.Attempts >= config.Attempts)
-                        {
-                            exMsg = "从异常中恢复,访问耗时 " + sw.ElapsedMilliseconds + "ms.";
-                            Debugger.WriteLine("{0} {1} {2}", this.Url.ToString(), this.Name, exMsg);
-                            await SendEmail(exMsg);
-                        }
-                        else
-                        {
-                            exMsg += ",访问耗时 " + sw.ElapsedMilliseconds + "ms.";
-                            Debugger.WriteLine("{0} {1} {2}", this.Url.ToString(), this.Name, exMsg);
-                            await TaskDelay();
-                        }
-                        webList.AddOrUpdate(this.Url, model, (key, newModel) => model);
-                        continue;
-                    }
-                    catch (WebException ex)
-                    {
-                        exMsg = ex.Message;
-                    }
-                    exMsg += ",访问耗时 " + sw.ElapsedMilliseconds + "ms.";
-                    model.State = false;
-                    //先查询是否有记录
-                    var exModel = webList.GetOrAdd(this.Url, model);
-                    exModel.Attempts += 1;
-                    exModel.State = false;
-                    if (exModel.Attempts < config.Attempts)
-                    {
-                        Debugger.WriteLine("{0} {1} {2} 正在重试 {3}!", this.Url.ToString(), this.Name, exMsg, exModel.Attempts);
-                        await Task.Delay(2000);
-                        continue;
-                    }
-                    webList.AddOrUpdate(this.Url, exModel, (key, oldState) => exModel);
+                #endregion
 
-                    Debugger.WriteLine("{0} {1} {2} 发送邮件!", this.Url.ToString(), this.Name, exMsg);
-                    await SendEmail(exMsg);
-                    await TaskDelay();
+                var exMsg = "正常";
+                string result = string.Empty;
+                //var model = new Webs() { Name = this.Name, Url = this.Url, Attempts = 0, State = true };
+                Stopwatch sw = new Stopwatch();  //运行时间
+                try
+                {
+                    sw.Reset();
+                    sw.Start();
+                    result = await client.DownloadStringTaskAsync(this.Url);
+                    sw.Stop();
+                    this.Attempts = 0;
+                    this.LastAlarmTime = null;
+                    this.NoticeInterval = 5;
+                    if (!this.State && this.Attempts >= config.Attempts)
+                    {
+                        exMsg = "从异常中恢复,访问耗时 " + sw.ElapsedMilliseconds + "ms.";
+                        Console.WriteLine("{0} {1} {2}", this.Url.ToString(), this.Name, exMsg);
+                        await SendEmail(exMsg);
+                    }
+                    else
+                    {
+                        exMsg += ",访问耗时 " + sw.ElapsedMilliseconds + "ms.";
+                        Console.WriteLine("{0} {1} {2}", this.Url.ToString(), this.Name, exMsg);
+                        await TaskDelay();
+                    }
+                    //var oldModel = webList.GetOrAdd(this.Url, this);
+                    //if (!oldModel.State && oldModel.Attempts >= config.Attempts)
+                    //{
+                    //    exMsg = "从异常中恢复,访问耗时 " + sw.ElapsedMilliseconds + "ms.";
+                    //    Console.WriteLine("{0} {1} {2}", this.Url.ToString(), this.Name, exMsg);
+                    //    await SendEmail(exMsg);
+                    //}
+                    //else
+                    //{
+                    //    exMsg += ",访问耗时 " + sw.ElapsedMilliseconds + "ms.";
+                    //    Console.WriteLine("{0} {1} {2}", this.Url.ToString(), this.Name, exMsg);
+                    //    await TaskDelay();
+                    //}
+                    //webList.AddOrUpdate(this.Url, this, (key, newModel) => this);
                     continue;
                 }
+                catch (WebException ex)
+                {
+                    exMsg = ex.Message;
+                }
+                exMsg += ",访问耗时 " + sw.ElapsedMilliseconds + "ms.";
+                this.State = false;
+                //先查询是否有记录
+                //var exModel = webList.GetOrAdd(this.Url, model);
+                this.Attempts += 1;
+                this.State = false;
+
+                if (this.Attempts < config.Attempts)
+                {
+                    Console.WriteLine("{0} {1} {2} 正在重试 {3}!", this.Url.ToString(), this.Name, exMsg, this.Attempts);
+                    await Task.Delay(2000);
+                    continue;
+                }
+
+                this.Attempts = 0;
+                //第一次时实发送邮件
+                if (!this.LastAlarmTime.HasValue)
+                {
+                    this.LastAlarmTime = DateTime.Now;
+                    await SendEmail(exMsg);
+                }
+                else
+                {
+                    //第二次发送判断与第一次的间隔时间
+                    if (DateTime.Now.Subtract(this.LastAlarmTime.Value).Minutes > this.NoticeInterval)
+                    {
+                        this.LastAlarmTime = DateTime.Now;
+                        this.NoticeInterval = this.NoticeInterval * 2;
+                        await SendEmail(exMsg);
+                    }
+                }
+                await TaskDelay();
+                continue;
             }
         }
 
@@ -127,13 +163,14 @@ namespace WebMonitor
         /// 发送邮件
         /// </summary>
         /// <param name="msg"></param>
+        /// <param name="IsForce">是否强制发送</param>
         /// <returns></returns>
         private async Task SendEmail(string msg)
         {
+            Console.WriteLine("{0} {1} {2} 发送邮件!", this.Url.ToString(), this.Name, msg);
             var sendModel = new MailContent(this.Name, this.Url.ToString(), msg);
             var pubs = new BastPush(sendModel);
             await pubs.SendAsync();
-            await TaskDelay();
         }
     }
 }
